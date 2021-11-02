21afd8e3fcc5a347b080be9ff55f1ca92cf80e56
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-02 08:56:28 -0300

    updated html

---
+++ assets/scripts/database.js
@@ -0,0 +1,141 @@
+
+
+
+let level = ['easy', 'medium', 'hard']
+let category = ['atualidade', 'literatura', 'ciencia', 'historia', 'geografia', 'logica', 'portugues']
+
+let questions = [
+    {
+        question: 'Qual o grupo em que todas as palavras foram escritas corretamente?',
+        level: 'easy',
+        category: 'portugues',
+        answers: {
+          a: 'Asterisco, beneficente, meteorologia, entretido',
+          b: 'Asterisco, beneficiente, metereologia, entretido',
+          c: 'Asterístico, beneficiente, metereologia, entretido',
+          d: 'Asterístico, beneficiente, meteorologia, entertido',
+        },
+        correctAnswer: 'a',
+    },
+    {
+        question: 'Qual o número mínimo de jogadores numa partida de futebol?',
+        level: 'easy',
+        category: 'atualidade',
+        answers: {
+          a: '8',
+          b: '9',
+          c: '7',
+          d: '10',
+        },
+        correctAnswer: 'c',
+    },
+    {
+        question: 'De quem é a famosa frase “Penso, logo existo"?',
+        level: 'easy',
+        category: 'literatura',
+        answers: {
+          a: 'Platão',
+          b: 'Galileu Galilei',
+          c: 'Descartes',
+          d: 'Sócrates',
+        },
+        correctAnswer: 'c',
+    },
+    {
+        question: 'De quem é a famosa frase “Penso, logo existo"?',
+        level: 'easy',
+        category: 'literatura',
+        answers: {
+          a: 'Platão',
+          b: 'Galileu Galilei',
+          c: 'Descartes',
+          d: 'Sócrates',
+        },
+        correctAnswer: 'c',
+    },
+    {
+        question: 'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+        level: 'easy',
+        category: 'historia',
+        answers: {
+          a: 'João Goulart',
+          b: 'João Figueiredo',
+          c: 'Getúlio Vargas',
+          d: 'Jânio Quadros',
+        },
+        correctAnswer: 'a',
+    },
+
+
+
+
+];
+
+/* 
+
+{ 
+    category: 'geografia'
+    level: 'medium'
+    question: 'Quais o menor e o maior país do mundo?'
+    answer-a: 'Mônaco e Canadá'
+    answerB: 'Malta e Estados Unidos'
+    answerC: 'Nauru e China'
+    answerD: 'Vaticano e Rússia'
+A: answerD
+}
+{ 
+    category: 'atualidade'
+    level: 'medium'
+    question: 'Qual o livro mais vendido no mundo a seguir à Bíblia?'
+    answer-a: 'O Senhor dos Anéis'
+    answerB: 'Dom Quixote'
+    answerC: 'O Pequeno Príncipe'
+    answerD: 'Harry Potter e a Pedra Filosofal'
+A: answerB
+}
+{ 
+    category: 'logica'
+    level: 'medium'
+    question: 'Quantas casas decimais tem o número pi?'
+    answer-a: 'Duas'
+    answerB: 'Milhares'
+    answerC: 'Vinte'
+    answerD: 'Infinitas'
+A: answerD
+}
+{ 
+    category: 'ciencia'
+    level: 'medium'
+    question: 'Atualmente, quantos elementos químicos a tabela periódica possui?'
+    answer-a: '113'
+    answerB: '118'
+    answerC: '109'
+    answerD: '108'
+A: answerB
+}
+{ 
+    category: 'portugues'
+    level: 'medium'
+    question: 'O que a palavra "legend" significa em português?'
+    answer-a: 'História'
+    answerB: 'Lenda'
+    answerC: 'Legenda'
+    answerD: 'Legendário'
+A: answerB
+}
+{ 
+    category: 'atualidades'
+    level: 'medium'
+    question: 'Quais os países que têm a maior e a menor expectativa de vida do mundo?'
+    answer-a: 'Japão e Serra Leoa'
+    answerB: 'Austrália e Afeganistão'
+    answerC: 'Estados Unidos e Angola'
+    answerD: 'Itália e Chade'
+A: answer-a
+}
+
+
+//fonte: https://www.todamateria.com.br/perguntas-e-respostas-de-conhecimentos-gerais/
+*/
+
+
---
+++ assets/scripts/game.js
@@ -0,0 +1,48 @@
+const question = document.getElementById('question')
+const answerA = document.getElementById('answerA')
+const answerB = document.getElementById('answerB')
+const answerC = document.getElementById('answerC')
+const answerD = document.getElementById('answerD')
+const submit = document.getElementById('submit')
+
+
+/* 
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		// code will go here
+	}
+
+	function showResults(questions, quizContainer, resultsContainer){
+		// code will go here
+	}
+
+	// show the questions
+	showQuestions(questions, quizContainer);
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
+
+function buildQuiz() {
+    const output = [];
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        const answers = [];
+        for (letter in currentQuestion.answers) {
+            answers.push(
+                
+            )
+        }
+    })
+}
+
+function showResults() {
+
+}
+
+buildQuiz()
+
+submitButton.addEventListener('click', showResults)
+*/
